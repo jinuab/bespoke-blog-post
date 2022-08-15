c522adc60ace20242abc5e50dbcdaac3752e8ca8
jinuab <69949958+jinuab@users.noreply.github.com>
2022-08-15 23:27:30 +0100

    blog post controller logic

--- app/Http/Controllers/BlogPostController.php
+++ app/Http/Controllers/BlogPostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\BlogPost;
+use App\Models\BlogPostComment;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -14,6 +15,7 @@ class BlogPostController extends Controller
 
         return view('blog.index', [
             'posts' => $posts,
+
         ]); //returns the view with posts
     }
 
@@ -40,17 +42,46 @@ class BlogPostController extends Controller
         return redirect('blog/' . $newPost->id);
     }
 
+    public function addComment(Request $request)
+    {
+        BlogPostComment::create([
+            'post_id' => $request->id,
+            'name' => $request->guest_name,
+            'comment' => $request->comment
+        ]);
+
+        return redirect('blog/' . $request->id);
+    }
+
     public function show(BlogPost $blogPost)
     {
+        $comments = BlogPostComment::where('post_id','=',$blogPost->id)->get(); // fetch all comments from DB
+        $user_details = json_decode($blogPost->user_id);
+
         return view('blog.viewDetails', [
             'post' => $blogPost,
+            'user_details' => $user_details,
+            'comments' => $comments
         ]); //returns the view with the post
     }
 
     public function edit(BlogPost $blogPost)
     {
+        $post_user_id = json_decode($blogPost->user_id)->id;
+
+        if($post_user_id != Auth::user()->id)
+        {
+            $allowEdit = 'No';
+        }
+        else
+        {
+            $allowEdit = 'Yes';
+        }
+
         return view('blog.edit', [
             'post' => $blogPost,
+            'postOwnerId' => $post_user_id,
+            'allowEdit' => $allowEdit,
         ]); //returns the edit view with the post
     }
 
